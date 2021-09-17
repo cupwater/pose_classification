c869dd76c88805e003d372dc665975a4a5f38b21
pengbaoyun <baoyun20@gmail.com>
2021-09-18 00:19:43 +0800

    add fc_B

--- models/fc_model.py
+++ models/fc_model.py
@@ -21,3 +21,33 @@ class fc_A(nn.Module):
         x = self.relu2(x)
         x = self.fc3(x)
         return x
+
+
+'''
+Author: Baoyun
+Date: 2021-09-12 
+Description: model
+'''
+import torch.nn as nn
+
+class fc_B(nn.Module):
+    def __init__(self, in_feature, class_num):
+        super(fc_A, self).__init__()
+        self.fc1 = nn.Linear(in_feature, 512)
+        self.relu1 = nn.ReLU()
+        self.fc2 = nn.Linear(512, 256)
+        self.relu2 = nn.ReLU()
+        self.fc3 = nn.Linear(256, 128)
+        self.relu3 = nn.ReLU()
+        self.fc4 = nn.Linear(128, class_num)
+        
+    def forward(self, x):
+        x = self.fc1(x)
+        x = self.relu1(x)
+        x = self.fc2(x)
+        x = self.relu2(x)
+        x = self.fc3(x)
+        x = self.relu3(x)
+        x = self.fc3(x)
+        return x
+
